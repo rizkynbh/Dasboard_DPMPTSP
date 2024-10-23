b095f510a9727e4c1d053db1959d5dd272eb267f
rizkynbh <rizky.naibahoo@gmail.com>
2024-10-23 17:58:38 +0700

    Update file excel dan script, serta hapus file yang tidak dibutuhkan

--- streamlit_investasi.py
+++ streamlit_investasi.py
@@ -2,9 +2,16 @@ import streamlit as st
 import pandas as pd
 
 # Baca data dari file Excel
-file_path = '/Users/macbookair/Desktop/Virtual Pyhton/Data Izin All - DPMPTSP - Dimas.xlsx'
+file_path = '/Users/macbookair/Desktop/Virtual Pyhton/Data Izin DPMPTSP.xlsx'
 sheet_name = 'Data Izin All'
 df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+
+# Jika 'total_revisi' ada, konversi tipe datanya
+if 'total_revisi' in df.columns:
+    df['total_revisi'] = pd.to_numeric(df['total_revisi'], errors='coerce').fillna(0).astype(int)
+
+
 
 st.title("Dashboard Perizinan Investasi di Indonesia")
 st.write("Data Perizinan Investasi Berdasarkan Wilayah")
@@ -13,11 +20,11 @@ st.write("Data Perizinan Investasi Berdasarkan Wilayah")
 st.dataframe(df)
 
 # Agregasi data
-total_izin = df['total_izin'].sum()
-average_izin = df['total_izin'].mean()
-selesai_diproses = df['selesai_diproses'].sum()
-ditolak_dibatalkan = df['ditolak_dibatalkan'].sum()
-masih_diproses = df['masih_diproses'].sum()
+total_izin = df['total_diajukan'].sum()  # Kolom total_diajukan
+average_izin = df['total_diajukan'].mean()
+selesai_diproses = df['total_selesai'].sum()  # Kolom total_selesai
+ditolak_dibatalkan = df['total_di_tolak'].sum() + df['total_dibatalkan'].sum()  # Kolom total_di_tolak + total_dibatalkan
+masih_diproses = df['total_proses'].sum()  # Kolom total_proses
 
 # Buat box untuk menampilkan data agregat
 st.metric("Total Izin", total_izin)
@@ -27,27 +34,24 @@ st.metric("Ditolak/Dibatalkan", ditolak_dibatalkan)
 st.metric("Masih Diproses", masih_diproses)
 
 # Dropdown untuk filter berdasarkan wilayah (kota/kecamatan)
-wilayah = st.selectbox('Pilih Wilayah', df['kecamatan'].unique())
+#wilayah = st.selectbox('Pilih Wilayah', df['Kec'].unique())  # Kolom Kec
 
 # Filter data berdasarkan wilayah yang dipilih
-filtered_df = df[df['kecamatan'] == wilayah]
+#filtered_df = df[df['Kec'] == wilayah]
 
 # Tampilkan data yang terfilter
-st.write(f"Data untuk {wilayah}")
-st.dataframe(filtered_df)
+#st.write(f"Data untuk {wilayah}")
+#st.dataframe(filtered_df)
 
-st.markdown("---")
+#st.markdown("---")
 
 import plotly.express as px
 
 # Buat pie chart untuk kategori izin per bidang
-pie_chart = px.pie(filtered_df, names='recode_bidang', title='Distribusi Kategori Izin Per Bidang')
-st.plotly_chart(pie_chart)
-
+#pie_chart = px.pie(filtered_df, names='Bidang_Recode', title='Distribusi Kategori Izin Per Bidang')  # Kolom Bidang_Recode
+#st.plotly_chart(pie_chart)
 
 # List bidang per kecamatan
-list_bidang = filtered_df['recode_bidang'].unique()
-st.write(f"Daftar Bidang di {wilayah}:")
-st.write(list_bidang)
-
-
+#list_bidang = filtered_df['Bidang_Recode'].unique()  # Kolom Bidang_Recode
+#st.write(f"Daftar Bidang di {wilayah}:")
+#st.write(list_bidang)
