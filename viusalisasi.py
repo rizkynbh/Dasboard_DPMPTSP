8eee4eb750439dfee795767bcc2aba46f5967952
rizkynbh <rizky.naibahoo@gmail.com>
2024-10-23 19:14:34 +0700

    versi dimas

--- viusalisasi.py
+++ viusalisasi.py
@@ -1,11 +1,19 @@
+import logging
+import streamlit as st
 import pandas as pd
 
-# Coba baca file Excel
+# Aktifkan logging untuk melihat lebih banyak detail tentang error
+logging.basicConfig(level=logging.DEBUG)
+
+# Baca data dari file Excel
 file_path = '/Users/macbookair/Desktop/Virtual Pyhton/Data Izin DPMPTSP.xlsx'
 sheet_name = 'Data Izin All'
-
 try:
-    df = pd.read_excel(file_path, sheet_name=sheet_name)
-    print(df.head())  # Cetak beberapa baris pertama untuk verifikasi
+    df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+    st.write("Data berhasil dibaca")
 except Exception as e:
-    print(f"Error saat membaca file Excel: {e}")
+    st.error(f"Terjadi error saat membaca file: {e}")
+    logging.error(f"Error saat membaca file Excel: {e}")
+
+st.title("Dashboard Perizinan Investasi di Indonesia")
+st.write("Data Perizinan Investasi Berdasarkan Wilayah")
